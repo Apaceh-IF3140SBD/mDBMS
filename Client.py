a7e47edabe0f85119d86075c4fb0eae055cdcb83
sotul04 <109208105+sotul04@users.noreply.github.com>
2024-12-13 16:52:30 +0700

    fix: client

--- Client.py
+++ Client.py
@@ -1,39 +1,76 @@
 import socket
 import json
+import traceback
 
 class Client:
     
     HOST = "localhost"
     PORT = 65432
     
-    def send_request(self, query: str = "", host: str = HOST, port: int = PORT):
+    def __init__(self):
+        self.begin_transaction = False
+        self.running = True
         try:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((host, port))
-            
-            client.send(query.encode('utf-8'))
-            
-            response = client.recv(1024*1024).decode('utf-8')
+            self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client.connect((Client.HOST, Client.PORT))
+        except Exception as e:
+            print("Error: ", e)
+            traceback.print_exc()
+            exit(1)
+    
+    def send_request(self, query: str = ""):
+        try:
+            self.client.send(query.encode('utf-8'))
+            response = self.client.recv(1024*1024).decode('utf-8')
             response_data = json.loads(response)
-            
-            if "header" in response_data:
-                print(response_data["header"])
-            
-            if "data" in response_data:
-                data = response_data["data"]
-                for row in data:
-                    print(row)
+            return response_data
         except Exception as e:
             print(f"Error: {e}")
-        finally:
-            client.close()
     
     def run(self):
-        while True:
-            query = input("Enter a query: ").strip()
-            if query.lower() == 'exit':
+        query_buffer = ""
+        prompt = "Apaceh=# "
+        while self.running:
+            try:
+                input_query = input(prompt).strip()
+                if input_query == r"\q":
+                    self.running = False
+                    print("Exiting...")
+                    break
+
+                query_buffer += input_query + ""
+
+                if query_buffer.endswith(";"):
+                    print(f"Executing query: {query_buffer.strip()}")
+                    query = query_buffer.strip().removesuffix(";")
+                    if query == "START TRANSACTION":
+                        self.begin_transaction = True
+                    elif query == 'COMMIT':
+                        self.begin_transaction = False    
+                    result = self.send_request(query)
+                    # if isinstance(result, list):  # SELECT queries
+                    #     print(format_as_table(result))
+                    # else:  # Other commands (INSERT, UPDATE)
+                    #     print(f"Message: {result}")
+                    print(result)
+                    query_buffer = ""
+                    prompt = "Apaceh=# "  
+                else:
+                    prompt = "Apaceh-# "
+            except Exception as e:
+                print(f"Error: {e}")
+            except KeyboardInterrupt:
+                print("\nKeyboard Interrupt detected. Exiting...")
+                if (self.begin_transaction):
+                    self.send_request("ABORT")
+                self.running = False
                 break
-            self.send_request(query)
+        self.client.close()
+        # while True:
+        #     query = input("Enter a query: ").strip()
+        #     if query.lower() == 'exit':
+        #         break
+        #     self.send_request(query)
     
 if __name__ == "__main__":
     Client().run()
@@ -64,72 +101,68 @@ def format_as_table(data):
 
 
 
-def initialize_storage(storage_engine):
-    student_schema = TableSchema(
-        table_name="students",
-        columns={
-            "StudentID": "int",
-            "FullName": "varchar(50)",
-            "GPA": "float"
-        }
-    )
-    storage_engine.create_table(student_schema)
+# def initialize_storage(storage_engine):
+#     student_schema = TableSchema(
+#         table_name="students",
+#         columns={
+#             "StudentID": "int",
+#             "FullName": "varchar(50)",
+#             "GPA": "float"
+#         }
+#     )
+#     storage_engine.create_table(student_schema)
 
-    # Insert test data
-    test_data = [
-        (1, "Alice", 3.9),
-        (2, "Bob", 3.2),
-        (3, "Charlie", 3.6),
-        (4, "Diana", 2.8)
-    ]
-    for student in test_data:
-        storage_engine.write_block(
-            data_write=DataWrite(
-                table="students",
-                columns=["StudentID", "FullName", "GPA"],
-                new_value=student,
-                conditions=[]
-            )
-        )
-    print("Test data inserted into 'students' table.")
-
-
-def get_query():
-    storage_engine = StorageEngine()
-    initialize_storage(storage_engine)  
-    query_processor = QueryProcessor(storage_engine)
-
-    dbms_on = True
-    query_buffer = ""
-    prompt = "Apaceh=# "
-    while dbms_on:
-        try:
-            input_query = input(prompt).strip()
-            if input_query == r"\q":
-                dbms_on = False
-                print("Exiting...")
-                break
-
-            query_buffer += input_query + ""
-
-            if query_buffer.endswith(";"):
-                print(f"Executing query: {query_buffer.strip()}")
-                result = query_processor.execute_query(query_buffer.strip())
-                if isinstance(result, list):  # SELECT queries
-                    print(format_as_table(result))
-                else:  # Other commands (INSERT, UPDATE)
-                    print(f"Message: {result}")
-                query_buffer = ""
-                prompt = "Apaceh=# "  
-            else:
-                prompt = "Apaceh-# "
-        except Exception as e:
-            print(f"Error: {e}")
-        except KeyboardInterrupt:
-            print("\nKeyboard Interrupt detected. Exiting...")
-            dbms_on = False
-            break
+#     # Insert test data
+#     test_data = [
+#         (1, "Alice", 3.9),
+#         (2, "Bob", 3.2),
+#         (3, "Charlie", 3.6),
+#         (4, "Diana", 2.8)
+#     ]
+#     for student in test_data:
+#         storage_engine.write_block(
+#             data_write=DataWrite(
+#                 table="students",
+#                 columns=["StudentID", "FullName", "GPA"],
+#                 new_value=student,
+#                 conditions=[]
+#             )
+#         )
+#     print("Test data inserted into 'students' table.")
 
 
-if __name__ == "__main__":
-    get_query()
+# def get_query():
+#     dbms_on = True
+#     query_buffer = ""
+#     prompt = "Apaceh=# "
+#     while dbms_on:
+#         try:
+#             input_query = input(prompt).strip()
+#             if input_query == r"\q":
+#                 dbms_on = False
+#                 print("Exiting...")
+#                 break
+
+#             query_buffer += input_query + ""
+
+#             if query_buffer.endswith(";"):
+#                 print(f"Executing query: {query_buffer.strip()}")
+#                 result = query_processor.execute_query(query_buffer.strip())
+#                 if isinstance(result, list):  # SELECT queries
+#                     print(format_as_table(result))
+#                 else:  # Other commands (INSERT, UPDATE)
+#                     print(f"Message: {result}")
+#                 query_buffer = ""
+#                 prompt = "Apaceh=# "  
+#             else:
+#                 prompt = "Apaceh-# "
+#         except Exception as e:
+#             print(f"Error: {e}")
+#         except KeyboardInterrupt:
+#             print("\nKeyboard Interrupt detected. Exiting...")
+#             dbms_on = False
+#             break
+
+
+# if __name__ == "__main__":
+#     get_query()
